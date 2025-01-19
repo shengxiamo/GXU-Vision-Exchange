ab96fa0ce7986903d828b4cf882e02a8b0e0c476
yangsj <13830695+ROBOT_Z@user.noreply.gitee.com>
2025-01-19 19:42:59 +0800

    添加moveit配置包

---
+++ rm_arm_2025_last/launch/view_robot.launch.py
@@ -0,0 +1,33 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    urdf_file = os.path.join(
+        get_package_share_directory('rm_arm_2025_last'),
+        'urdf',
+        'rm_arm_2025_last.urdf')
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': open(urdf_file).read()}]
+        ),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
+            output='screen'
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', os.path.join(get_package_share_directory('rm_arm_2025_last'), 'rviz', 'view_robot.rviz')]
+        )
+    ])
