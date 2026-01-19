6a4c093698f2e05bf22da8e00dd93f5eadb4e28b
yangsj <13830695+ROBOT_Z@user.noreply.gitee.com>
2026-01-20 00:18:52 +0800

    暂时注释掉串口打开和数据发送的代码，以便进行测试

--- rm_arm_hardware/src/rm_arm_hardware_interface.cpp
+++ rm_arm_hardware/src/rm_arm_hardware_interface.cpp
@@ -75,6 +75,8 @@ hardware_interface::CallbackReturn RMArmHardwareInterface::on_configure(
     const rclcpp_lifecycle::State & previous_state)
 {
     (void)previous_state;
+    
+    // 为测试暂时注释掉串口打开代码
     // try {
     //     serial_driver_->init_port(device_name_, *device_config_);
     //     if (!serial_driver_->port()->is_open()) {
@@ -109,16 +111,15 @@ hardware_interface::return_type RMArmHardwareInterface::read(
     const rclcpp::Time & /*time*/, const rclcpp::Duration & period)
 {
     // 4. 在 read 中把接收到的 packet 数据更新到 ros2_control 的状态向量中
-    // 假设关节顺序与 packet 定义顺序一致 (joint1, joint2, ...)
     if (hw_states_position_.size() >= 6) {
-        // hw_states_position_[0] = received_packet_.joint1;
-        // hw_states_position_[1] = received_packet_.joint2;
-        // hw_states_position_[2] = received_packet_.joint3;
-        // hw_states_position_[3] = received_packet_.joint4;
-        // hw_states_position_[4] = received_packet_.joint5;
-        // hw_states_position_[5] = received_packet_.joint6;
+        hw_states_position_[0] = received_packet_.joint1;
+        hw_states_position_[1] = received_packet_.joint2;
+        hw_states_position_[2] = received_packet_.joint3;
+        hw_states_position_[3] = received_packet_.joint4;
+        hw_states_position_[4] = received_packet_.joint5;
+        hw_states_position_[5] = received_packet_.joint6;
 
-        // test
+        // 以下代码仅为测试，使用时删除
         std::vector<double> pre_states_potion = hw_states_position_;
 
         hw_states_position_[0] = hw_commands_[0];
@@ -142,7 +143,7 @@ hardware_interface::return_type RMArmHardwareInterface::read(
 hardware_interface::return_type RMArmHardwareInterface::write(
     const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/)
 {
-    
+    //为测试暂时注释掉发送数据的代码
     // send_packet_.joint1 = hw_commands_[0];
     // send_packet_.joint2 = hw_commands_[1];
     // send_packet_.joint3 = hw_commands_[2];
@@ -155,6 +156,7 @@ hardware_interface::return_type RMArmHardwareInterface::write(
 
     // 序列化并发送
     std::vector<uint8_t> data = rm_serial_driver::toVector(send_packet_);
+    // 为测试暂时注释掉发送数据的代码
     // try {
     //     serial_driver_->port()->send(data);
     // } catch (const std::exception & ex) {
